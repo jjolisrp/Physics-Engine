bbde8fa7770caaa12b1ceae996bb4a3b02476a1c
Jose Luis <165792842+jjolisrp@users.noreply.github.com>
2024-12-20 09:21:10 +0100

    Fixed 100%

--- PhysicsEngine/Game/Main.cpp
+++ PhysicsEngine/Game/Main.cpp
@@ -1,19 +1,23 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Mi primer ventana SFML");
-    sf::CircleShape circle(50.0f);
-    circle.setFillColor(sf::Color::Green);
+    // Crear una ventana con tamaño 800x600 y título "Prueba SFML"
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Prueba SFML");
 
+    // Bucle principal para mantener la ventana abierta
     while (window.isOpen()) {
+        // Crear un evento para gestionar las acciones del usuario
         sf::Event event;
         while (window.pollEvent(event)) {
+            // Si el evento es de tipo "cerrar la ventana", salir del bucle
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(circle);
+        // Limpiar la ventana con el color negro
+        window.clear(sf::Color::Black);
+
+        // Mostrar lo que hemos dibujado (en este caso nada)
         window.display();
     }
 
